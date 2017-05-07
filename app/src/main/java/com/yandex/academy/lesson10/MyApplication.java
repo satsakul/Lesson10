65e63be7600e95f7d7c145bdf13769422bab02a4
Stanislav Kulikov <satsakul@yandex-team.ru>
2017-05-08 00:56:51 +0300

    strict_mode1

---
+++ app/src/main/java/com/yandex/academy/lesson10/MyApplication.java
@@ -0,0 +1,24 @@
+package com.yandex.academy.lesson10;
+
+import android.app.Application;
+import android.os.StrictMode;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    //.penaltyDialog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+        }
+
+        super.onCreate();
+    }
+}
